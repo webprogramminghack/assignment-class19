804d8abec3d0cede8d9349c5873ac49f32ff4da0
Dion <prep.prodigy1@gmail.com>
2025-02-22 16:28:57 +0700

    Perbaikan string sesuai desain figma dan menhapus clsx karena hanya 1 style

--- src/components/Dialog/Dialog.tsx
+++ src/components/Dialog/Dialog.tsx
@@ -1,14 +1,12 @@
 import styles from './Dialog.module.scss';
 import React from 'react';
-import clsx from 'clsx';
 import { Button } from '@/components/Button';
 import ImageSuccess from '@/assets/svg/icon-success.svg';
 import ImageInfo from '@/assets/svg/icon-info.svg';
-import ImageDanger from '@/assets/svg/icon-danger.svg';
 
 // Define the type for props passed to the Alert component
 interface AlertProps {
-  variant: 'success' | 'info' | 'danger'; // Define the variant type
+  variant: 'success' | 'info'; // Define the variant type
   onClose: () => void;
   onConfirm: () => void;
 }
@@ -20,52 +18,44 @@ export const Dialog: React.FC<AlertProps> = ({
 }) => {
   const dialogProps = {
     success: {
-      title: 'Successfully loaded',
-      description: 'Neque porro quisquam est qui dolorem ipsum',
+      title: 'Successfully created a new user',
+      description: 'The new user has been successfully created',
       confirmValue: 'Ok',
     },
     info: {
-      title: 'New information',
-      description: 'Neque porro quisquam est qui dolorem ipsum',
+      title: 'Do you want to create a new user?',
+      description: 'Click the create button to continue',
       confirmValue: 'Create',
-      isDisabled: false,
-    },
-    danger: {
-      title: 'Are you sure you want to delete this user?',
-      description: 'This action is irreversible',
-      confirmValue: 'Delete',
       isDisabled: false,
     },
   };
 
   return (
-    <div className={clsx(styles.dialogContainer)}>
-      <div className={clsx(styles.alertContainer)}>
-        <div className={clsx(styles.alertContent)}>
-          <div className={clsx(styles.alertIcon)}>
+    <div className={styles.dialogContainer}>
+      <div className={styles.alertContainer}>
+        <div className={styles.alertContent}>
+          <div className={styles.alertIcon}>
             {(() => {
               switch (variant) {
                 case 'success':
-                  return <ImageSuccess className={clsx(styles.icon)} />;
+                  return <ImageSuccess className={styles.icon} />;
                 case 'info':
-                  return <ImageInfo className={clsx(styles.icon)} />;
-                case 'danger':
-                  return <ImageDanger className={clsx(styles.icon)} />;
+                  return <ImageInfo className={styles.icon} />;
                 default:
                   return null;
               }
             })()}
           </div>
-          <div className={clsx(styles.alertText)}>
-            <h3 className={clsx(styles.title)}>{dialogProps[variant].title}</h3>
-            <p className={clsx(styles.description)}>
+          <div className={styles.alertText}>
+            <h3 className={styles.title}>{dialogProps[variant].title}</h3>
+            <p className={styles.description}>
               {dialogProps[variant].description}
             </p>
           </div>
         </div>
 
-        <div className={clsx(styles.footer)}>
-          <div className={clsx(styles.content)}>
+        <div className={styles.footer}>
+          <div className={styles.content}>
             {/* conditional rendering, https://react.dev/learn/conditional-rendering# */}
             {variant === 'success' && (
               <Button color='secondary' onClick={onClose}>
